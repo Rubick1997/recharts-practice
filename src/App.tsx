cc7a8152f934bb2b7b0763b076788ba6868f3cb8
Rustam Kolumbayev <rustamkolumbayev@Rustams-MacBook-Pro.local>
2021-08-13 18:03:11 -0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -35,34 +35,49 @@ const data = [
 
 function App() {
   return (
-    <ResponsiveContainer width="100%" aspect={3}>
-      <LineChart
-        width={500}
-        height={300}
-        data={data}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid
-          strokeDasharray="3 3"
-          horizontal={true}
-          vertical={true}
-        />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Line
-          type="monotone"
-          dataKey="iphone"
-          stroke="#8884d8"
-          activeDot={{ r: 8 }}
-        />
-      </LineChart>
-    </ResponsiveContainer>
+    <>
+      <h2>Quartelry sales figures for Iphones</h2>
+      <ResponsiveContainer width="99%" aspect={3}>
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 15,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid
+            strokeDasharray="3 3"
+            horizontal={true}
+            vertical={true}
+            stroke="#243240"
+          />
+          <XAxis dataKey="name" tick={{ fill: "#fff" }} />
+          <YAxis tick={{ fill: "#fff" }} />
+          <Tooltip
+            contentStyle={{ backgroundColor: "#8884d8", color: "#fff" }}
+            itemStyle={{ color: "#fff" }}
+            cursor={true}
+          />
+          <Line
+            type="monotone"
+            dataKey="iphone"
+            stroke="#8884d8"
+            strokeWidth="5"
+            dot={{ fill: "2e4355", stroke: "#8884d8", strokeWidth: 2, r: 5 }}
+            activeDot={{
+              fill: "2e4355",
+              stroke: "#8884d8",
+              strokeWidth: 5,
+              r: 10,
+            }}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </>
   );
 }
 
